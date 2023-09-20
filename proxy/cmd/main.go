a6d240ea88bfc90a522fe438747ec54ccf29c412
Toni Tienda <tonitienda@gmail.com>
2023-09-21 07:30:35 +0900

    feat: render basic version of current node in marmaid format

---
+++ proxy/cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tonitienda/go-proxy-mermaid/templates"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		data := templates.GetServiceData()
+
+		fmt.Println("Requesting", c.Request.URL.Path)
+		switch c.NegotiateFormat(gin.MIMEHTML, gin.MIMEJSON) {
+		case gin.MIMEHTML:
+			page := data.GetPage()
+
+			c.Data(200, "text/html; charset=utf-8", []byte(page))
+		case gin.MIMEJSON:
+			c.JSON(200, data)
+		}
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
